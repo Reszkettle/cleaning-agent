b749d7852b5d739862e39e2082f8bf0a3847fb42
Jakub Reszka <reszkettle@gmail.com>
2019-08-20 14:56:00 +0200

    Changed: move_files is now returning amount of moved files, make_directories is now checking if there is a need of organizing.

--- folder_functions.py
+++ folder_functions.py
@@ -7,15 +7,25 @@ from tkinter import Tk
 
 def make_directories(folders, path):
     os.chdir(path=path)
+    list_items = os.listdir(path=path)
+    list_length = len(list_items)
+    counter = 0
+    for item in list_items:
+        if os.path.isdir(item) or item.endswith('.ini'):
+            counter += 1
+    if counter == list_length:
+        return 0
     try:
         for folder in folders:
             if not os.path.isdir('./' + folder):
                 os.mkdir('./' + folder)
     except NotImplementedError:
         print('dir_fd is not implemented on this computer')
+    return 1
 
 
 def move_files(path):
+    moved = 0
     for f in os.listdir(path):
         downloads_prefix = path + '/'
         current_dir = downloads_prefix + f
@@ -27,6 +37,8 @@ def move_files(path):
                     renamed_path = downloads_prefix + 'copy_' + f
                     os.rename(current_dir, renamed_path)
                     shutil.move(renamed_path, downloads_prefix + key)
+                moved += 1
+    return moved
 
 
 def get_folder(title="Choose folder to organize"):
@@ -34,5 +46,4 @@ def get_folder(title="Choose folder to organize"):
     root.iconbitmap('icon.ico')
     root.withdraw()
     path = filedialog.askdirectory(title=title)
-    print(path)
     return path
