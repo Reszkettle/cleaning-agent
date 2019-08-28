80b439fb49c533a27bc04c71c3314fc522b8a13e
Jakub Reszka <reszkettle@gmail.com>
2019-08-28 18:00:49 +0200

    Added few changes in catalogs.py

--- catalogs.py
+++ catalogs.py
@@ -1,16 +1,22 @@
 extensions = {
-    'Videos': ('.mp4', '.avi', '.flv', '.mpg', '.mp2', '.m4v', '.3gp', '.mkv', '.vob', '.mkv', '.drc', '.gif', '.gifv', '.mng', '.mts', '.m2ts', '.ts', '.mov', '.wmv',
-               '.yuv', '.rmvb', '.asf', '.amv', '.rmvb', '.mpg', '.mpeg', '.mp2', '.mpv', '.m2v', '.mp4',
-               '.m4p', '.m4v', '.svi', '.3gp', '.3g2', '.mxf', '.roq', '.nsv', '.f4v', '.f4p', '.f4a',
-               '.f4b'),
-    'Photos': ('.jpg', '.png', '.jpeg', '.bmp', '.ico'),
-    'Archives': ('.zip', '.rar', '.tar', '.tz'),
-    'Executables': ('.exe', '.bat', '.msi', '.jar'),
-    'Text Files': ('.txt', '.doc', '.odt', '.pdf', '.xlsx', '.xlsm', '.xls', '.xlt', '.rtf', '.pptx', '.odp'),
-    'Music': ('.3gp', '.aa', '.aac', '.aax', '.act', '.aiff', '.alac', '.amr', '.ape', '.au', '.awb', '.dct',
-              '.dss', '.dvf', '.flac', '.gsm', '.iklax', '.ivs', '.m4a', '.m4b', '.m4p', '.mmf', '.mp3', '.mpc',
-              '.msv', '.nmf', '.nsf', '.ogg', '.oga', '.mogg', '.opus', '.ra', '.rm', '.raw', '.sln', '.tta',
-              '.voc', '.vox', '.wav', '.wma', '.wv', '.webm', '.8svx'),
-    'Books': ('.mobi', '.epub')
+    'Videos': (
+        '.mp4', '.avi', '.flv', '.mpg', '.mp2', '.m4v', '.3gp', '.mkv', '.vob', '.mkv', '.drc', '.gif', '.gifv', '.mng', '.mts',
+        '.m2ts', '.ts', '.mov', '.wmv', '.yuv', '.rmvb', '.asf', '.amv', '.rmvb', '.mpg', '.mpeg', '.mp2', '.mpv', '.m2v', '.mp4',
+        '.m4p', '.m4v', '.svi', '.3gp', '.3g2', '.mxf', '.roq', '.nsv', '.f4v', '.f4p', '.f4a', '.f4b'),
+    'Photos': (
+        '.jpg', '.png', '.jpeg', '.bmp', '.ico', '.webp', '.webm'),
+    'Archives': (
+        '.zip', '.rar', '.tar', '.tz', '.iso'),
+    'Executables': (
+        '.exe', '.bat', '.msi', '.jar'),
+    'Text Files': (
+        '.txt', '.doc', '.odt', '.xlsx', '.xlsm', '.xls', '.xlt', '.rtf', '.pptx', '.odp', '.bin', '.ini'),
+    'Music': (
+        '.3gp', '.aa', '.aac', '.aax', '.act', '.aiff', '.alac', '.amr', '.ape', '.au', '.awb', '.dct',
+        '.dss', '.dvf', '.flac', '.gsm', '.iklax', '.ivs', '.m4a', '.m4b', '.m4p', '.mmf', '.mp3', '.mpc',
+        '.msv', '.nmf', '.nsf', '.ogg', '.oga', '.mogg', '.opus', '.ra', '.rm', '.raw', '.sln', '.tta',
+        '.voc', '.vox', '.wav', '.wma', '.wv', '.webm', '.8svx'),
+    'Books and PDF': (
+        '.mobi', '.epub', '.pdf')
 }
 folder_names = [key for key in extensions.keys()]
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from folder_functions import *
+from folder_functions import make_directories, move_files, get_folder, folder_names
 
 path = get_folder()
 if path is not None:
